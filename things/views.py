cfc21f5b7f82cf088c028e551216e05466bed661
LorenzoC <lorenzo.cataldi@icloud.com>
2023-11-30 16:16:39 +0000

    Check Things app

---
+++ things/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("<html><head><title>Things</title></head><body><h1>Things</h1></body></html>")
